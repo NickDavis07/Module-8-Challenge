dba9663ca98edb297ecf281d28f623fd26fb1c40
Nick Davis <nickwagner@live.com>
2024-12-12 19:34:35 -0700

    Final Commit maybe?

--- src/classes/Truck.ts
+++ src/classes/Truck.ts
@@ -30,8 +30,8 @@ class Truck extends Vehicle {
     year: number,
     weight: number,
     topSpeed: number,
-    towingCapacity: number,
-    wheels: Wheel[] = []
+    wheels: Wheel[] = [],
+    towingCapacity: number
   ) {
     // TODO: The constructor should call the constructor of the parent class, Vehicle
     super();
@@ -43,13 +43,14 @@ class Truck extends Vehicle {
     this.year = year;
     this.weight = weight;
     this.topSpeed = topSpeed;
-    this.towingCapacity = towingCapacity;
-    // TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
     if (wheels.length !== 4) {
       this.wheels = [new Wheel(), new Wheel(), new Wheel(), new Wheel()];
     } else {
       this.wheels = wheels;
     }
+    this.towingCapacity = towingCapacity;
+    // TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
+   
   }
 
   // TODO: Implement the tow method from the AbleToTow interface
